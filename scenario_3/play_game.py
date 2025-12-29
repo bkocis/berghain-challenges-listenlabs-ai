eefe64ec09dffc4ff20ccb2111b38cb6a070b2ce
Balazs.Kocsis <balaz.kocis@gmail.com>
2025-12-29 02:46:56 +0100

    107de4d9-e818-443e-b678-7841cf5cc37a

--- scenario_3/play_game.py
+++ scenario_3/play_game.py
@@ -253,13 +253,13 @@ def should_accept_person(
     
     # HARD RULES
 
-    if queer_count < 180:
+    if queer_count < 130:
         if is_queer:
             return True
         else:
             return False
     
-    if vinyl_count < 180:
+    if vinyl_count < 130:
         if is_vinyl:
             return True
         else:
